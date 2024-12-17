12ebee956f6b96ee4feb43f9f109d77b4d391da8
BOOPESH-foxy <boopesh.mc@gmail.com>
2024-12-17 21:37:59 +0530

    cloudwatch

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,12 +1,34 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_cloudwatch_log_group" "backend_log_group" {
+  name = "/aws/ec2/backend"
+}
+
+resource "aws_cloudwatch_metric_alarm" "high_cpu_alarm" {
+  alarm_name          = "HighCPUUsage"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 300
+  statistic           = "Average"
+  threshold           = 80
+
+  dimensions = {
+    InstanceId = aws_instance.backend.id
+  }
+}
+
 # EC2 Instance - Backend
 resource "aws_instance" "backend" {
-  ami               = "ami-0e306788ff2473ccb"  # Amazon Linux 2 AMI
+  ami               = "ami-0e306788ff2473ccb"  
   instance_type     = var.instance_type
   key_name          = var.key_name
   availability_zone = "ap-south-1a"
   subnet_id         = aws_subnet.public_subnet.id
 
-  # User data to install Docker and backend setup
   user_data = <<-EOT
               #!/bin/bash
               sudo yum update -y
@@ -14,6 +36,38 @@ resource "aws_instance" "backend" {
               sudo service docker start
               sudo usermod -aG docker ec2-user
               sudo yum install -y git
+              sudo yum install -y aws-cfn-bootstrap
+              sudo yum install -y amazon-cloudwatch-agent
+
+              echo '{
+                "agent": {
+                  "metrics_collection_interval": 60,
+                  "run_as_user": "root"
+                },
+                "metrics": {
+                  "append_dimensions": {
+                    "InstanceId": "${!instance_id}"
+                  },
+                  "aggregation_dimensions": [["InstanceId"]],
+                  "metrics_collected": {
+                    "CPU": {
+                      "measurement": ["UsageIdle", "UsageUser", "UsageSystem"],
+                      "metrics_collection_interval": 60
+                    },
+                    "Disk": {
+                      "measurement": ["Used", "Free", "Total"],
+                      "metrics_collection_interval": 60
+                    },
+                    "Memory": {
+                      "measurement": ["MemUsed", "MemAvailable", "MemTotal"],
+                      "metrics_collection_interval": 60
+                    }
+                  }
+                }
+              }' > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+
+              sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a start -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+
               cd /home/ec2-user
               git clone https://github.com/BOOPESH-foxy/Hello_DevOps.git
               cd Hello_DevOps/backend
@@ -28,6 +82,7 @@ resource "aws_instance" "backend" {
   vpc_security_group_ids = [aws_security_group.backend_sg.id]
 }
 
+# Security Group for Backend
 resource "aws_security_group" "backend_sg" {
   name        = "backend-sg"
   description = "Allow HTTP and SSH traffic for backend"
@@ -38,7 +93,7 @@ resource "aws_security_group" "backend_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -46,7 +101,7 @@ resource "aws_security_group" "backend_sg" {
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
